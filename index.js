735d6f1d6ee3bccf7e6574fea4f99d3d26c06776
tronlives <benbelghith@gmx.com>
2023-05-25 00:16:52 +0200

    Update index.js

--- index.js
+++ index.js
@@ -24,6 +24,35 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+// timestamp API
+app.get('/api/:date?', (req, res) => {
+ const dateString = req.params.date
+ const dateStringRegex = /^[0-9]+$/
+ const numbersOnly = dateStringRegex.test(dateString)
+
+ if (!numbersOnly) {
+   const unixTimestamp = Date.parse(dateString)
+   const utcDate = new Date(unixTimestamp).toUTCString()
+
+   unixTimestamp
+   ? res.json({ unix: unixTimestamp, utc: utcDate })
+   : res.json({ error: "Invalid Date" })
+ } 
+ else {
+   const unixTimestamp = parseInt(dateString)
+   const actualDate = new Date(unixTimestamp)
+   const utcDate = actualDate.toUTCString()
+
+   res.json({ unix: unixTimestamp, utc: utcDate })
+ }
+
+  app.get('/api', (req, res) => {
+    const currentDate = new Date().toUTCString()
+    const currentUnix = Date.parse(currentDate)
+    res.json({ unix: currentUnix, utc: currentDate })
+  })
+})
+
 
 
 // listen for requests :)
